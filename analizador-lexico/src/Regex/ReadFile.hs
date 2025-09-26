32bc1fbdb1e7b17d53e38fac4f290252391e49e4
paolasanv <sanv.leslie@ciencias.unam.mx>
2025-09-25 21:18:39 -0600

    Las regex del archivo de texto ahora se unen en una sola regex

--- analizador-lexico/src/Regex/ReadFile.hs
+++ analizador-lexico/src/Regex/ReadFile.hs
@@ -4,6 +4,7 @@ module Regex.ReadFile where
 import Regex.Lexer
 import Regex.Parser
 
+import Data.List (intercalate)
 import System.FSNotify
 import System.FilePath (takeFileName, takeDirectory)
 import Control.Concurrent (threadDelay)
@@ -18,19 +19,17 @@ import System.Directory (doesFileExist)
 toRegex :: String -> RegEx 
 toRegex x = parser $ lexer x
 
--- Convierte un texto en una lista de cadenas, separando por saltos de línea
-text2Lines :: T.Text -> [String]
-text2Lines contenido = map T.unpack (T.lines contenido)
+-- Convierte un texto en una lista de cadenas, separando por el símbolo de concatenación
+text2String :: T.Text -> String
+text2String content = intercalate " | " ( map T.unpack (T.lines content))
 
 -- Función que se ejecuta cuando el archivo es modificado
 processFIle :: FilePath -> IO ()
 processFIle path = do
-    contenido <- TIO.readFile path
+    content <- TIO.readFile path
     putStrLn "\nArchivo procesado. Actualizando las expresiones regulares obtenidas"
-    let lineas = text2Lines contenido      
-    let expresionesRegulares = map toRegex lineas
-    putStrLn "Expresiones regulares:"
-    print expresionesRegulares
+    putStrLn "Expresion regular"
+    print(toRegex (text2String content))
 
 -- Funcion principal
 file2RegEx :: FilePath -> IO ()
