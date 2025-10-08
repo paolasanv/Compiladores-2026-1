bc38a93a3a2fa2bd6bdccfba93053b265990fe6a
MildredMtz <mildred27@ciencias.unam.mx>
2025-10-08 17:52:39 -0600

    Solo se cambiaron unas líneas para evitar errores de tipo con la nueva definición de deltaHat

--- analizador-lexico/app/Main.hs
+++ analizador-lexico/app/Main.hs
@@ -1,7 +1,104 @@
 module Main (main) where
 
+<<<<<<< Updated upstream
 import Regex.ReadFile
 import Automatas.NFA_E(toNFAE)  
+=======
+import System.Console.Haskeline
+import Control.Monad.IO.Class (liftIO)
+import Control.Concurrent (threadDelay, forkIO, MVar, newMVar, modifyMVar_, readMVar)
+import Control.Monad (forever, void)
+
+import Regex.Lexer
+import Regex.Parser
+import MDD (lexerDo)
+import Automatas.NFA_E (toNFAE)
+import Automatas.NFA (toNFA)
+import Automatas.DFA (toDFA)
+import Automatas.DFA_min (minimize)
+
+import Data.List (intercalate)
+import System.FSNotify
+import System.FilePath (takeFileName, takeDirectory)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Paths_analizador_lexico (getDataFileName)
+import System.Directory (doesFileExist)
+
+
+-- Conversión de archivo a expresión regular
+toRegex :: String -> RegEx
+toRegex x = parser $ lexer x
+
+text2String :: T.Text -> String
+text2String content = intercalate " | " (map T.unpack (T.lines content))
+
+loadRegexFromFile :: IO RegEx
+loadRegexFromFile = do
+    existeLocal <- doesFileExist "regex.txt"
+    ruta <- if existeLocal then return "regex.txt" else getDataFileName "regex.txt"
+    contenido <- T.readFile ruta
+    let regex = toRegex (text2String contenido)
+    putStrLn $ "[Actualización] " ++ show regex
+    return regex
+
+-- Vigilar cambios en el archivo regex.txt
+watchRegexFile :: MVar RegEx -> IO ()
+watchRegexFile ref = withManager $ \mgr -> do
+    existeLocal <- doesFileExist "regex.txt"
+    ruta <- if existeLocal then return "regex.txt" else getDataFileName "regex.txt"
+    let carpeta = takeDirectory ruta
+    let archivo = takeFileName ruta
+
+    -- Observador de cambios
+    void $ watchDir
+        mgr
+        carpeta
+        (\e -> case e of
+            Modified path _ _ -> takeFileName path == archivo
+            _ -> False)
+        (\_ -> do
+            putStrLn $ "\nCambio detectado en " ++ archivo
+            regex' <- loadRegexFromFile
+            modifyMVar_ ref (const (return regex'))
+        )
+    forever $ threadDelay 1000000
+
+
+repl :: MVar RegEx -> IO ()
+repl ref = runInputT defaultSettings loop
+  where
+    loop :: InputT IO ()
+    loop = do
+      minput <- getInputLine "> "
+      case minput of
+        Nothing -> return ()
+        Just "" -> loop
+        Just ":q" -> liftIO (putStrLn "Chao :)")
+        Just xs ->
+          if take 5 xs == "lexer"
+            then do
+              let rawArgs = drop 6 xs
+              let args = stripQuotes rawArgs
+              regex <- liftIO $ readMVar ref
+              liftIO $ do
+                putStrLn "[Ejecución] Usando expresión regular actual:"
+                print regex
+                putStrLn $ "lexer se llama con: " ++ args
+                let afd = minimize $ toDFA $ toNFA $ toNFAE regex
+                print (lexerDo afd args)
+              loop
+            else do
+              liftIO $ putStrLn $ "Comando desconocido: " ++ xs
+              loop
+
+    -- Elimina comillas dobles al inicio y final, si existen
+    stripQuotes :: String -> String
+    stripQuotes s =
+      case s of
+        ('"':rest) | not (null rest) && last rest == '"' -> init rest
+        _ -> s
+>>>>>>> Stashed changes
 
 main :: IO ()
 main =  file2RegEx "regex.txt"
--- analizador-lexico/src/Automatas/DFA.hs
+++ analizador-lexico/src/Automatas/DFA.hs
@@ -1,6 +1,7 @@
 {-|
 Module      : Automatas.DFA
 Description : Autómatas finitos deterministas.
+Author: Paola Mildred Martinez Hidalgo.
 
 Este módulo implementa el algoritmo de conversión de un 
 autómata finito no determinista (AFN) a un autómata finitos determinista (AFD).
@@ -8,6 +9,7 @@ autómata finito no determinista (AFN) a un autómata finitos determinista (AFD)
 module Automatas.DFA where
 
 import Automatas.NFA_E (State)
+<<<<<<< Updated upstream
 import Automatas.NFA (NFA)
 import qualified Automatas.NFA as NFA
 import qualified Data.Set as Set
@@ -15,6 +17,13 @@ import Data.Set (Set)
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.List (foldl')
+=======
+import Automatas.NFA (NFA(..))
+import qualified Data.Set as Set
+import Data.Set (Set)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+>>>>>>> Stashed changes
 
 type DeltaDFA = (State, Char, State)
 -- Debido a que los estados en el DFA son CONJUNTOS de estados 
@@ -29,6 +38,7 @@ data DFA = DFA {
     final       :: [State]
 } deriving (Show)
 
+<<<<<<< Updated upstream
 -- Pasa un NFA a un DFA usando el algoritmo de subconjutos
 toDFA :: NFA -> DFA
 toDFA  nfa = 
@@ -119,3 +129,65 @@ computeDestination currentSet symbol nfa =
 
 
 
+=======
+-- Construye un DFA a partir de un NFA (sin ε-transitions)
+toDFA :: NFA -> DFA
+toDFA nfa =
+    let
+        -- Se mantiene el estado inicial del NFA
+        startSet = Set.singleton (startNFA nfa)
+
+        -- Devuelve los estados alcanzables desde un conjunto de estados con un símbolo
+        move :: Set State -> Char -> Set State
+        move estados simbolo =
+            Set.unions [ Set.fromList qs
+                       | (q, a, qs) <- Set.toList (transitionsNFA nfa)
+                       , q `Set.member` estados
+                       , a == simbolo
+                       , not (null qs)
+                       ]
+
+        -- Construcción recursiva de los estados del DFA
+        build :: [Set State] -> Set (Set State) -> Map.Map (Set State, Char) (Set State)
+              -> (Set (Set State), Map.Map (Set State, Char) (Set State))
+        build [] visitados delta = (visitados, delta) -- si no hay más edos por procesar, devuelve los visitados y las transiciones
+        build (actual:cola) visitados delta =
+            let alfabeto = Set.toList (alphabetNFA nfa)
+                movimientos = [ (a, move actual a) | a <- alfabeto, not (Set.null (move actual a)) ] --indica para donde moverse con cada símbolo
+                nuevos = [ s | (_, s) <- movimientos, not (s `Set.member` visitados) ] -- estados que aún no se visitan
+                delta' = foldr (\(a,s) acc -> Map.insert (actual,a) s acc) delta movimientos -- actualiza las transiciones
+            in build (cola ++ nuevos) (Set.union visitados (Set.fromList nuevos)) delta'
+
+        -- Cada conjunto de estados del NFA se representa por el estado mínimo del conjunto.
+        representative :: Set State -> State
+        representative s = Set.findMin s
+
+        (allStates, deltaMap) = build [startSet] (Set.singleton startSet) Map.empty
+
+        -- Convierte el Map de transiciones en un Set DeltaDFA
+        deltaDFA = Set.fromList
+          [ (representative qSet, a, representative qSet')
+          | ((qSet,a), qSet') <- Map.toList deltaMap
+          ]
+
+        -- Calcula los estados finales del DFA, es final si contiene un estado final del NFA 
+        finals = [ representative s
+                 | s <- Set.toList allStates
+                 , any (`elem` finalNFA nfa) (Set.toList s)
+                 ]
+    -- Construye el DFA final 
+    in DFA {
+        states = Set.map representative allStates,
+        alphabet = alphabetNFA nfa,
+        transitions = deltaDFA,
+        start = representative startSet,
+        final = finals
+    }
+
+-- Transición extendida de un DFA
+deltaHat :: DFA -> State -> Char -> Maybe State
+deltaHat dfa q c =
+  case [q' | (q1, a, q') <- Set.toList (transitions dfa), q1 == q, a == c] of
+    (q':_) -> Just q'
+    []     -> Nothing
+>>>>>>> Stashed changes
